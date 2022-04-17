cb79c13d9ac1b94bd8e7babf0a876f10a963584f
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-04-18 00:09:41 +0600

    blog completed

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import authImg from 'https://www.okta.com/sites/default/files/styles/1640w_scaled/public/media/image/2020-10/Authentication_vs_Authorization.png?itok=uBFRCfww';
+
+import firbaseServiceImg from '../../images/firebaseServices.jpg';
+
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            <div className="first-ques">
-                <h3>Authentication vs. authorization</h3>
+        <div className='row blog-area'>
+            <div className="common-des first-ques col-sm-12 col-md-6">
+                <h2>Authentication vs. authorization</h2>
                 <p>
                     Authenctication is a simple process which can verify someone's identity. On the otherhand, in authorization process it verifies the specific application ,files, and data  a user can access or not. We can compare it like an airplane. At first airlines company have to check the identity of the passengers for being sure who are they actually. After determining the passengers identity, secondly they cheack if this passenger has any special service like vip lounge or first class seat.
                     <br />
                     In the world of modern technology these kinds of works is done by authorization and authenctication. Authentication confirms the real identity of a person actually who they are. After confirming the identity authorization process is start to cheack if the user have permission to access the different level of that application and commits specific task depending on the role.     
                 </p>
-                <h2>Why i am using firebase?</h2>
+                
+
+            </div>
+            <div className="common-des first-img col-sm-12 col-md-6">
+                <img src="https://www.okta.com/sites/default/files/styles/1640w_scaled/public/media/image/2020-10/Authentication_vs_Authorization.png?itok=uBFRCfww" alt="" />
+            </div>
+            <div className="common-des sec-img col-sm-12 col-md-6">
+                <img src="https://miro.medium.com/max/1400/1*JbDo7U0l62vYMfm1WxnA1g.png" alt="" />
+            </div>
+            <div className="common-des second-Ques col-sm-12 col-md-6">
+            <h2>Why i am using firebase?</h2>
                 <p>
                     I use firebase because 
                     Firebase Authentication makes it easier to get my users signed-in without having to understand the complexities behind implementing my own authentication system.
@@ -31,10 +44,9 @@ const Blogs = () => {
                         <li>Grant</li>
                     </ul>
                 </p>
-
             </div>
 
-            <div className="last-ques">
+            <div className="common-des last-ques col-sm-12 col-md-6">
                 <h2>Other Services provided by firebase</h2>
                 <p>
                 Firebase is a full package that can help in developing your mobile or web applications faster with fewer resources and more efficiency. Now, let's look at some of the services and use of it.
@@ -55,6 +67,9 @@ const Blogs = () => {
 
                     Firebase is a full package that can help in developing your mobile or web applications faster with fewer resources and more efficiency. Now, let's look at some of the services and use of it.
                 </p>
+            </div>
+            <div className="common-des last-img col-sm-12 col-md-6">
+                <img src={firbaseServiceImg} alt="" />
             </div>
         </div>
     );
