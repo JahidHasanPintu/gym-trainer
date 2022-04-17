7f411c8cb34fc941ee1173fd841ffa2f39070e81
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-04-17 22:14:16 +0600

    footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,24 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
+        <div className='footer'>
             <footer>
-               <h1>This is footer</h1>
-                                                     
-            </footer>
+            <div class="footer-container btm-footer">
+                <div class="row margin-btm-tp-3">
+                    <div class="col-md-12 col-sm-12 col-xs-12 text-center footer-icons xs-center-text xs-margin-btm-1"> 
+                        <a target="_blank" href="#"> <i class="fa fa-facebook"></i> </a> 
+                        <a target="_blank" href="#"> <i class="fa fa-twitter"></i> </a> 
+                        <a target="_blank" href="#"> <i class="fa fa-youtube"></i> </a> 
+                        <a target="_blank" href="#"> <i class="fa fa-linkedin"></i> </a>
+                    </div>
+                </div>
+                 <div class="row">
+                    <p class="text-center">All rights reserved - 2022<a href="#">©Pintu - FitnessTrainer</a></p></div></div> 
+                                                        
+                </footer>
+            
+               
+               
                                         
         </div>
     );
