ce94134cf6d8fcdb96cd30f89d062ca491971971
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-04-18 10:45:29 +0600

    project recovered

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,14 +5,24 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey : process.env.REACT_APP_apiKey,
+//   authDomain : process.env.REACT_APP_authDomain,
+//   projectId : process.env.REACT_APP_projectId,
+//   storageBucket : process.env.REACT_APP_storageBucket,
+//   messagingSenderId : process.env.REACT_APP_messagingSenderId,
+//   appId : process.env.REACT_APP_appId,
+// };
+
 const firebaseConfig = {
-  apiKey : process.env.REACT_APP_apiKey,
-  authDomain : process.env.REACT_APP_authDomain,
-  projectId : process.env.REACT_APP_projectId,
-  storageBucket : process.env.REACT_APP_storageBucket,
-  messagingSenderId : process.env.REACT_APP_messagingSenderId,
-  appId : process.env.REACT_APP_appId,
+  apiKey: "AIzaSyAGPEFwj3cIN86dGonBZ3oE0dV2Oso7ik0",
+  authDomain: "gymtrainerwebsite.firebaseapp.com",
+  projectId: "gymtrainerwebsite",
+  storageBucket: "gymtrainerwebsite.appspot.com",
+  messagingSenderId: "197426188420",
+  appId: "1:197426188420:web:8354cce94b56085bd4b2d1"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
