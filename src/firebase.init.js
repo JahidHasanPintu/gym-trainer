aaf9faf03d6a61314951dc7122605209e5627305
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-04-18 08:08:04 +0600

    set environment variable for auth

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGPEFwj3cIN86dGonBZ3oE0dV2Oso7ik0",
-  authDomain: "gymtrainerwebsite.firebaseapp.com",
-  projectId: "gymtrainerwebsite",
-  storageBucket: "gymtrainerwebsite.appspot.com",
-  messagingSenderId: "197426188420",
-  appId: "1:197426188420:web:8354cce94b56085bd4b2d1"
+  apiKey : process.env.REACT_APP_apiKey,
+  authDomain : process.env.REACT_APP_authDomain,
+  projectId : process.env.REACT_APP_projectId,
+  storageBucket : process.env.REACT_APP_storageBucket,
+  messagingSenderId : process.env.REACT_APP_messagingSenderId,
+  appId : process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
